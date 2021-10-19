8b959ee4baade7b195a6e68d4bb95914c3f5ca15
Serge Krstic <srdjankrstic@gmail.com>
2021-10-19 15:13:18 +1100

    Add react fast refresh for development

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <section className="hero"></section>
       <main>
         <section>
-          <h1>Oh Hai, React</h1>
+          <h1>Oh Hai, React dfsfd</h1>
         </section>
         <img src={sword} alt="sword" width="250px" />
         <img src={bannerSvg} alt="bannerSvg" width="250px" />
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
 const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 let mode = 'development';
 
+const plugins = [
+  new CleanWebpackPlugin(),
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+];
+
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
+} else {
+  plugins.push(new ReactRefreshWebpackPlugin());
 }
 
 module.exports = {
   mode: mode,
+
+  // entry: './src/index.js',
 
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -52,13 +65,7 @@ module.exports = {
     ],
   },
 
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
+  plugins: plugins,
 
   resolve: {
     extensions: ['.js', '.jsx'],
